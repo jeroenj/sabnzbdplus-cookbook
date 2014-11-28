cb97fa8f9a449ccad319e19526b0a9b79c40ceb5
Jeroen Jacobs <git@jeroenj.be>
2014-11-28 23:16:05 +0100

    Bumps version to v1.0.2

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer        'Jeroen Jacobs'
 maintainer_email  'chef@jeroenj.be'
 license           'MIT'
 description       'Installs sabnzbd.'
-version           '1.0.1'
+version           '1.0.2'
 
 depends           'apt', '>= 2.6.0'
 
